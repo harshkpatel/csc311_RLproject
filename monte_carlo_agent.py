52133b27b3217659073e958b791f7062cd90f85d
harshkpatel <hkpatel1760@gmail.com>
2023-12-06 00:02:23 -0500

    weights

--- monte_carlo_agent.py
+++ monte_carlo_agent.py
@@ -8,8 +8,34 @@ def random_run(game):
     while not game_copy.game_end: 
         move = random.randint(0, 3)
         game_copy.make_move(move)
-    
-    return game_copy.get_sum(), game_copy.max_num(), game_copy.get_merge_score()
+
+    heuristic_score = board_heuristic(game_copy)
+    return game_copy.get_sum(), game_copy.max_num(), game_copy.get_merge_score(), heuristic_score
+
+def get_adjacent_tiles(board, row, col):
+    adjacent = []
+    if row > 0: adjacent.append(board[row-1][col])
+    if row < 3: adjacent.append(board[row+1][col])
+    if col > 0: adjacent.append(board[row][col-1])
+    if col < 3: adjacent.append(board[row][col+1])
+    return adjacent
+
+def board_heuristic(game):
+    score = 0
+    board = game.matrix
+    max_tile = max(map(max, board))
+    second_max = sorted(set(sum(board, [])), reverse=True)[1]  # Get the second-highest tile
+
+    for i in range(4):
+        for j in range(4):
+            if board[i][j] == max_tile:
+                score += max_tile * 20  # Weighting factor for max tile in corner
+                adjacent_tiles = get_adjacent_tiles(board, i, j)
+
+                # Check if second-max tiles are adjacent to the max tile
+                score += sum(tile for tile in adjacent_tiles if tile == second_max) * 7.5 # Weighting factor
+
+    return score
 
 def monte_carlo_iter(game):
     total_score = [0, 0, 0, 0]
@@ -24,18 +50,19 @@ def monte_carlo_iter(game):
         # Try lots of paths with that move using random rollout policy
         for i in range(NUM_ITERS):
             output = random_run(game_copy)   # 0 for largest tile, 1 for sum
+            heuristic_bonus = output[3] # Weights
             
             # Eval Method 0: Best total sum
             if EVAL_METHOD == 0:
-                total_score[move] += output[0]
+                total_score[move] += output[0] + heuristic_bonus
 
             # Eval Method 1: Largest tile number
             elif EVAL_METHOD == 1:
-                total_score[move] += output[1]
+                total_score[move] += output[1] + heuristic_bonus
 
             # Eval Method 2: Largest total merge score
             elif EVAL_METHOD == 2:
-                total_score[move] += output[2]
+                total_score[move] += output[2] + heuristic_bonus
 
     best_move = total_score.index(max(total_score))
     game.make_move(best_move)
@@ -58,8 +85,8 @@ def main():
     global NUM_TRIALS 
     global EVAL_METHOD
 
-    NUM_ITERS = 30
-    NUM_TRIALS = 10
+    NUM_ITERS = 100
+    NUM_TRIALS = 5
     EVAL_METHOD = 0
     # Eval method 0: sum of all tiles
     # Eval method 1: largest tiles on board, then sum of all tiles if tied largest tile
